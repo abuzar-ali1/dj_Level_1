300f25572571eabd06958e05b63105a60ccbb689
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-25 00:58:31 +0500

    Craeted a greeting APi for user

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('home/' , views.home , {"message" : "THis is the HOme page and THis is comming form the passing the data to the Url"})
+    path('home/' , views.home , {"message" : "THis is the HOme page and THis is comming form the passing the data to the Url"}),
+    path('api/greeting/<username>/' , views.greeting)
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , get_object_or_404
+from django.http import HttpResponse , JsonResponse
 # Create your views here.
 
 
 def home(request , message):
     return HttpResponse(f"<h1>{message}</h2>")
+
+
+
+
+def greeting(request , username):
+    result = {'message' : f'Welcome back {username}' , 'is_admin' : False}
+    return JsonResponse(result)
+
