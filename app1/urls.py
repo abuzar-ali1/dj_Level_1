270b8d584132950aebeaf92d60df9378798173d7
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-24 23:15:44 +0500

    DOne the level_1

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('home/' , views.home , {"message" : "THis is the HOme page and THis is comming form the passing the data to the Url"})
+]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.
+from django.http import HttpResponse
 # Create your views here.
 
 
-def home(request):
+def home(request , message):
+    return HttpResponse(f"<h1>{message}</h2>")
