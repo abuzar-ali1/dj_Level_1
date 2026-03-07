961a3f50b6c9bf57bfca0c2b6fcc6b02e7292076
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-03-07 21:25:46 +0500

    register at admin

--- app2/admin.py
+++ app2/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile , HackathonIdea
+from .models import Profile , HackathonIdea ,Teacher
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(HackathonIdea)
+admin.site.register(Teacher)
+admin.site.register(Teacher)
